e0870fce953e01f0fba827f86e0d3ee67dd5663d
Dmitriy Borovikov <dmitry@borovikov.name>
2025-12-27 19:07:30 +0300

    Tesks and  factory partition scripts refactoring

--- makeFactoryPartition.sh
+++ makeFactoryPartition.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-partition_file=$(find -quit ./factory_partition -name "*-partition.bin" 2>/dev/null)
+partition_file=$(find ./factory_partition -name "*-partition.bin" -print -quit 2>/dev/null)
 if [[ ${#partition_file} != 0 ]]; then
     echo Generated $partition_file already exist. Cleanup and retry script.
     exit 1
 fi
-source ./factory.sh
-MATTER_SDK_PATH=$ESP_MATTER_PATH/connectedhomeip/connectedhomeip
+source ./factoryData.sh
+MATTER_CRED_PATH=$ESP_MATTER_PATH/connectedhomeip/connectedhomeip/credentials/test
 echo Make partition binary
 esp-matter-mfg-tool --outdir factory_partition \
     --target "$IDF_TARGET" \
@@ -20,6 +20,6 @@ esp-matter-mfg-tool --outdir factory_partition \
     --product-url "$PRODUCT_URL" \
     --pai \
     --cn-prefix "LightWC" \
-    --key "$MATTER_SDK_PATH/credentials/test/attestation/Chip-Test-PAI-$VENDOR_ID-$PRODUCT_ID-Key.pem" \
-    --cert "$MATTER_SDK_PATH/credentials/test/attestation/Chip-Test-PAI-$VENDOR_ID-$PRODUCT_ID-Cert.pem" \
-    --cert-dclrn "$MATTER_SDK_PATH/credentials/test/certification-declaration/Chip-Test-CD-$VENDOR_ID-$PRODUCT_ID.der"
+    --key "$MATTER_CRED_PATH/attestation/Chip-Test-PAI-$VENDOR_ID-$PRODUCT_ID-Key.pem" \
+    --cert "$MATTER_CRED_PATH/attestation/Chip-Test-PAI-$VENDOR_ID-$PRODUCT_ID-Cert.pem" \
+    --cert-dclrn "$MATTER_CRED_PATH/certification-declaration/Chip-Test-CD-$VENDOR_ID-$PRODUCT_ID.der"
