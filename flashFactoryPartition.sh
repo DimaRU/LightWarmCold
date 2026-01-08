beb17fa4a73f87d904f0a0e0af0d6f94466b7264
Dmitriy Borovikov <dmitry@borovikov.name>
2026-01-08 19:14:30 +0300

    Update tasks ans scripts for multiple factory partitions make & flash

---
+++ flashFactoryPartition.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+factory_partition_folder=${FACTORY_PARTITION:-"./factory_partition"}
+
+files=()
+while IFS= read -r file; do
+  files+=("$file")  # store full path
+done < <(find $factory_partition_folder -name "*-partition.bin" | sort)
+
+# Exit if no files found
+if (( ${#files[@]} == 0 )); then
+  echo "No factory partition binary files found."
+  exit 1
+fi
+
+index=1
+for file in "${files[@]}"; do
+  echo "$index) $(basename "$file")"
+  ((index++))
+done
+
+read -rp "Enter selection number: " idx
+
+# Validate input
+if [[ "$idx" =~ ^[0-9]+$ ]] && (( idx >= 1 && idx <= ${#files[@]} )); then
+  factory_partition_file=${files[idx-1]}
+else
+  echo "Invalid selection."
+  exit 1
+fi
+
+echo "Flash $factory_partition_file"
+echo "parttool.py --port $1 --partition-table-file partitions.csv write_partition --partition-name=\"factory\" --input=$factory_partition_file"
--- makeFactoryPartition.sh
+++ makeFactoryPartition.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
-partition_file=$(find ./factory_partition -name "*-partition.bin" -print -quit 2>/dev/null)
+
+factory_partition_path=${FACTORY_PARTITION:-"./factory_partition"}
+
+partition_file=$(find $factory_partition_path -name "*-partition.bin" -print -quit 2>/dev/null)
 if [[ ${#partition_file} != 0 ]]; then
-    echo Generated $partition_file already exist. Cleanup and retry script.
+    echo Generated $partition_file already exist. Please cleanup and retry.
     exit 1
 fi
-source ./factoryData.sh
+
+if [[ -z "${FACTORY_PARTITION}" ]]; then
+    source ./factoryData.sh
+else
+    source $FACTORY_PARTITION/factoryData.sh
+fi
+
+read -rp "Enter count of manufacturing partition binaries to generate: " count
+
+if ! [[ "$count" =~ ^[1-9][0-9]*$ ]]; then
+    echo "Error: input must be a number greater than zero."
+    exit 1
+fi
+
 MATTER_CRED_PATH=$ESP_MATTER_PATH/connectedhomeip/connectedhomeip/credentials/test
-echo Make partition binary
-esp-matter-mfg-tool --outdir factory_partition \
+esp-matter-mfg-tool --outdir $factory_partition_path \
+    --cn-prefix "LightWC" \
+    --count $count \
     --target "$IDF_TARGET" \
     --vendor-id 0x$VENDOR_ID \
     --vendor-name "$VENDOR_NAME" \
@@ -15,11 +32,9 @@ esp-matter-mfg-tool --outdir factory_partition \
     --product-name "$PRODUCT_NAME" \
     --hw-ver $HW_VER \
     --hw-ver-str "$HW_VER_STR" \
-    --serial-num $SERIAL_NUM \
     --product-label "$PRODUCT_LABEL" \
     --product-url "$PRODUCT_URL" \
     --pai \
-    --cn-prefix "LightWC" \
     --key "$MATTER_CRED_PATH/attestation/Chip-Test-PAI-$VENDOR_ID-$PRODUCT_ID-Key.pem" \
     --cert "$MATTER_CRED_PATH/attestation/Chip-Test-PAI-$VENDOR_ID-$PRODUCT_ID-Cert.pem" \
     --cert-dclrn "$MATTER_CRED_PATH/certification-declaration/Chip-Test-CD-$VENDOR_ID-$PRODUCT_ID.der"
