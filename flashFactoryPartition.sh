ef3ff6df8e5b07776c419a5e8ff1f861c92d13e1
Dmitriy Borovikov <dmitry@borovikov.name>
2026-01-14 23:07:04 +0300

    Refactoring flashFactoryPartition script - show serial numbers, etc

--- flashFactoryPartition.sh
+++ flashFactoryPartition.sh
@@ -1,32 +1,44 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 factory_partition_folder=${FACTORY_PARTITION:-"./factory_partition"}
 
-files=()
-while IFS= read -r file; do
-  files+=("$file")  # store full path
-done < <(find $factory_partition_folder -name "*-partition.bin" | sort)
+CSV_FILE=$(find $factory_partition_folder -name "summary-*")
 
-# Exit if no files found
-if (( ${#files[@]} == 0 )); then
-  echo "No factory partition binary files found."
-  exit 1
-fi
+files=()
+serials=()
+qrcodes=()
+manualcodes=()
+{
+    read
+    while IFS=',' read -r -a columns
+    do
+        files+=("${columns[10]}")
+        serials+=("${columns[9]}")
+        qrcodes+=("${columns[19]}")
+        manualcodes+=("${columns[20]}")
+    done
+} < "$CSV_FILE"
 
 index=1
-for file in "${files[@]}"; do
-  echo "$index) $(basename "$file")"
+for serial in "${serials[@]}"; do
+  echo "$index) $serial"
   ((index++))
 done
 
-read -rp "Enter selection number: " idx
+read -rp "Select serial to flash: " idx
 
-# Validate input
-if [[ "$idx" =~ ^[0-9]+$ ]] && (( idx >= 1 && idx <= ${#files[@]} )); then
-  factory_partition_file=${files[idx-1]}
+if [[ "$idx" =~ ^[1-9][0-9]*$ ]] && (( idx <= ${#files[@]} )); then
+  factory_partition_file=${files[idx-1]%/*/*}
 else
   echo "Invalid selection."
   exit 1
 fi
+
+last_segment="${factory_partition_file##*/}"
+factory_partition_file=${factory_partition_file}\/${last_segment}-partition.bin
+
+echo "qrcode:" ${qrcodes[idx-1]}
+echo "manual code:" ${manualcodes[idx-1]}
+echo "partition binary:" ${factory_partition_file}
 
 parttool.py --port $1 --partition-table-file partitions.csv write_partition --partition-name="factory" --input=$factory_partition_file
